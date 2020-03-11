e79d5069d864f138926a40ab0d42fcdbaa6cc9e5
yinyinyinyinyin <348029293@qq.com>
2020-03-11 11:55:54 +0800

    aa

--- vue-xm-shop/shop/src/router/index.js
+++ vue-xm-shop/shop/src/router/index.js
@@ -1,23 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+//import Home from '../views/home/index.vue'
+//引入框架页面
+import Layout from '../views/layout';
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
+  {path:'/login',component:()=>import(/*webpackChunkName:"login"*/'@/views/login/index')},
+  {path:'/404',component:()=>import(/*webpackChunkName:"404"*/'@/views/404')},
+  {path:'',component:Layout,redirect:'/home',name:'home',
+  children:[
+	  {
+		path:'home',
+		name:'myhome',
+		component:()=>import(/*webpackChunkName:"home"*/'@/views/home/index')  
+	  }
+	  
+  ]
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
 ]
 
 const router = new VueRouter({
---
+++ vue-xm-shop/shop/src/views/layout/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<div>左侧</div>
+		<div>
+			<div>上</div>
+			<router-view></router-view>
+			
+		</div>
+	
+	</div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
